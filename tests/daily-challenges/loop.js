66da8f8d9af2b428e11e99c72c7f5b4567c05a66
phatle21 <patrick.le.204@gmail.com>
2025-09-18 17:19:41 +0700

    i have completed some challeges

---
+++ tests/daily-challenges/loop.js
@@ -0,0 +1,10 @@
+var count = 0;
+while (count < 5) {
+    console.log("Count is: ".concat(count));
+    count++;
+}
+var fruits = ["Apple", "Banana", "Cherry"];
+for (var _i = 0, fruits_1 = fruits; _i < fruits_1.length; _i++) {
+    var fruit = fruits_1[_i];
+    console.log(fruit);
+}
